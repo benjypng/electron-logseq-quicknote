deb64ad871588889656e9a922b6bde7f79a540ba
hkgnp <ben@pngs.cc>
2022-11-28 12:57:30 +0800

    First commit

---
+++ main.js
@@ -0,0 +1,31 @@
+const { app, BrowserWindow, globalShortcut } = require("electron");
+const path = require("path");
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 450,
+    height: 100,
+    webPreferences: {
+      nodeIntegration: true,
+      preload: path.join(__dirname, "src/script.js"),
+    },
+  });
+  win.loadFile("index.html");
+}
+
+app.whenReady().then(function () {
+  createWindow();
+});
+
+app.on("ready", () => {
+  globalShortcut.register("CommandOrControl+L", createWindow);
+});
+
+app.on("window-all-closed", function (e) {
+  e.preventDefault();
+  e.returnValue = false;
+});
+
+app.on("will-quit", function () {
+  globalShortcut.unregisterAll();
+});
---
+++ src/script.js
@@ -0,0 +1,20 @@
+const { shell } = require("electron");
+
+window.addEventListener("DOMContentLoaded", function () {
+  const quickNote = document.getElementById("quickNote");
+  quickNote.focus();
+
+  quickNote.addEventListener("keydown", function (e) {
+    if (e.key === "Enter") {
+      if (quickNote.value === "") {
+        alert("Please key in something");
+        return;
+      } else {
+        shell.openExternal(
+          `logseq://x-callback-url/quickCapture?url=&title=&append=true&page=TODAY&content=${quickNote.value}`
+        );
+        quickNote.value = "";
+      }
+    }
+  });
+});
