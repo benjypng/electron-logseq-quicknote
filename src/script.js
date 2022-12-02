c8db99900f0956f37c0005330e2ee16b2b6b4164
unknown <ben@pngs.cc>
2022-12-02 21:24:05 +0800

    fixed special characters not capturing

--- src/script.js
+++ src/script.js
@@ -1,19 +1,21 @@
-const { shell } = require("electron");
+const { shell } = require('electron');
 
-window.addEventListener("DOMContentLoaded", function () {
-  const quickNote = document.getElementById("quickNote");
+window.addEventListener('DOMContentLoaded', function () {
+  const quickNote = document.getElementById('quickNote');
   quickNote.focus();
 
-  quickNote.addEventListener("keydown", function (e) {
-    if (e.key === "Enter") {
-      if (quickNote.value === "") {
-        alert("Please key in something");
+  quickNote.addEventListener('keydown', function (e) {
+    if (e.key === 'Enter') {
+      if (quickNote.value === '') {
+        alert('Please key in something');
         return;
       } else {
         shell.openExternal(
-          `logseq://x-callback-url/quickCapture?url=&title=&append=true&page=TODAY&content=${quickNote.value}`
+          `logseq://x-callback-url/quickCapture?url=&title=&append=true&page=TODAY&content=${encodeURIComponent(
+            quickNote.value
+          )}`
         );
-        quickNote.value = "";
+        quickNote.value = '';
       }
     }
   });
