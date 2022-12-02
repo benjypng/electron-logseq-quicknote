ffe4802fdb6dfbd0d3ddd917f1a6f81c900e0ef8
unknown <ben@pngs.cc>
2022-12-02 21:52:26 +0800

    Added support for specifying page

--- main.js
+++ main.js
@@ -1,23 +1,23 @@
-const { app, Menu, Tray, BrowserWindow, globalShortcut } = require("electron");
-const path = require("path");
+const { app, Menu, Tray, BrowserWindow, globalShortcut } = require('electron');
+const path = require('path');
 
 function createWindow() {
   const win = new BrowserWindow({
-    width: 450,
-    height: 150,
+    width: 500,
+    height: 200,
     webPreferences: {
       nodeIntegration: true,
-      preload: path.join(__dirname, "src/script.js"),
+      preload: path.join(__dirname, 'src/script.js'),
     },
   });
-  win.loadFile("index.html");
+  win.loadFile('index.html');
 
-  win.on("minimize", function (e) {
+  win.on('minimize', function (e) {
     e.preventDefault();
     win.hide();
   });
 
-  win.on("close", function (e) {
+  win.on('close', function (e) {
     if (!app.isQuiting) {
       e.preventDefault();
       win.hide();
@@ -28,28 +28,28 @@ function createWindow() {
 
 app.whenReady().then(function () {
   createWindow();
-  const tray = new Tray(path.join(__dirname, "src/icons/capture.png"));
+  const tray = new Tray(path.join(__dirname, 'src/icons/capture.png'));
   const contextMenu = Menu.buildFromTemplate([
     {
-      label: "Quit",
+      label: 'Quit',
       click: function () {
         app.exit(0);
       },
     },
   ]);
-  tray.setToolTip("Logseq Quick Note");
+  tray.setToolTip('Logseq Quick Note');
   tray.setContextMenu(contextMenu);
 });
 
-app.on("ready", () => {
-  globalShortcut.register("CommandOrControl+L", createWindow);
+app.on('ready', () => {
+  globalShortcut.register('CommandOrControl+L', createWindow);
 });
 
-app.on("window-all-closed", function (e) {
+app.on('window-all-closed', function (e) {
   e.preventDefault();
   e.returnValue = false;
 });
 
-app.on("will-quit", function () {
+app.on('will-quit', function () {
   globalShortcut.unregisterAll();
 });
--- src/script.js
+++ src/script.js
@@ -2,6 +2,7 @@ const { shell } = require('electron');
 
 window.addEventListener('DOMContentLoaded', function () {
   const quickNote = document.getElementById('quickNote');
+  const page = document.getElementById('page');
   quickNote.focus();
 
   quickNote.addEventListener('keydown', function (e) {
@@ -11,9 +12,9 @@ window.addEventListener('DOMContentLoaded', function () {
         return;
       } else {
         shell.openExternal(
-          `logseq://x-callback-url/quickCapture?url=&title=&append=true&page=TODAY&content=${encodeURIComponent(
-            quickNote.value
-          )}`
+          `logseq://x-callback-url/quickCapture?url=&title=&append=true&page=${encodeURIComponent(
+            page.value === '' ? 'TODAY' : page.value
+          )}&content=${encodeURIComponent(quickNote.value)}`
         );
         quickNote.value = '';
       }
